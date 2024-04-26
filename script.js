2435d2ee8fb9bdf386a8d3fd4fa59e36db543e37
rsonaniya <rajatsonaniya6@gmail.com>
2024-04-26 17:59:16 +0530

    Corrected Code

--- script.js
+++ script.js
@@ -5,11 +5,30 @@ let longitude;
 let latitude;
 let map;
 
-function GetMap() {
+function loadMapScript() {
+  if (typeof Microsoft !== "undefined") {
+    initializeMap();
+  } else {
+    var scriptUrl = "https://www.bing.com/api/maps/mapcontrol?key=YOUR_API_KEY&callback=initializeMap";
+
+    let script = document.createElement("script");
+    script.type = "text/javascript";
+    script.async = true;
+    script.defer = true;
+    script.src = scriptUrl;
+
+    document.body.appendChild(script);
+  }
+}
+
+function initializeMap() {
   map = new Microsoft.Maps.Map("#myMap", {
     credentials: "Air - qpW_C0y - zs3ixctgYQwqcozndID7AezO5e05nTS2XEiXQf13Wu4oweVO8m2o",
   });
 }
+
+loadMapScript();
+
 const fetchLocation = () => {
   loading.style.display = "block";
   navigator.geolocation.getCurrentPosition(
@@ -29,18 +48,3 @@ const fetchLocation = () => {
 };
 
 getLocation.addEventListener("click", fetchLocation);
-
-// function GetMap() {
-//   var map = new Microsoft.Maps.Map("#myMap", {
-//     credentials: "Air - qpW_C0y - zs3ixctgYQwqcozndID7AezO5e05nTS2XEiXQf13Wu4oweVO8m2o",
-//   });
-
-//   navigator.geolocation.getCurrentPosition(function (position) {
-//     var loc = new Microsoft.Maps.Location(position.coords.latitude, position.coords.longitude);
-
-//     var pin = new Microsoft.Maps.Pushpin(loc);
-//     map.entities.push(pin);
-
-//     map.setView({ center: loc, zoom: 15 });
-//   });
-// }
